93896e828c8bcdeea109528637100a6551c730c6
ThabisoMathebula <thabiso.mathebula@yahoo.com>
2025-07-29 14:28:24 +0200

    Updated the home page

--- Home.py
+++ Home.py
@@ -6,17 +6,16 @@ st.set_page_config(page_title="Absaluminum".upper(), page_icon=abs_icon)
 
 st.image("Images/Heading_Letter_head.png")
 
-st.title("📄 Absaluminum Quotation App")
-st.markdown("Welcome to the official quotation generator for **Absaluminum Projects**.")
+st.title("Absaluminum (PTY) LTD".upper())
+st.markdown("Welcome to the official quotation and invoice generator for **Absaluminum Projects**.")
 
 st.divider()
 
-st.header("Client Details")
-st.text_input("Client Name")
-st.text_input("Contact Number")
+st.header("Invoice Summary")
+
 
 st.divider()
 
-st.header("Quotation Information")
-st.number_input("Total Price", min_value=0.0)
-st.date_input("Quotation Date")
+st.header("Quotation Summary")
+
+
